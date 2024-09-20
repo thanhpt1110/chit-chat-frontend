93bd702f51ec2a5c155de03d03284adc496669d3
Tuan-Thanh <thanhphantuan1110@gmail.com>
2024-09-20 12:00:00 +0700

    chore: setup env file and test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		open: true,
-	},
+export default defineConfig(({ mode }) => {
+	// Load environment variables based on the mode
+	dotenv.config({ path: `.env.${mode}` });
+
+	return {
+		plugins: [react()],
+		server: {
+			open: true,
+		},
+	};
 });
